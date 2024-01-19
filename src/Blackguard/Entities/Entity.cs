03b0ef5c8c00ee71e878cb1f427679a9d58345f8
LandonLeo <landon.beert@gmail.com>
2024-01-19 12:44:32 -0600

    Fix spacing

--- src/Blackguard/Entities/Entity.cs
+++ src/Blackguard/Entities/Entity.cs
@@ -4,19 +4,33 @@ namespace Blackguard.Entities;
 
 public abstract class Entity {
     public struct DefaultsStruct {
+
         public int MaxMana;
+
         public int MaxHealth;
+
         public int MaxSpeed;
+
         public double BluntEffect;
+
         public double SlashEffect;
+
         public double PierceEffect;
+
         public double MagicEffect;
+
         public double BaseEffect;
+
         public double FireEffect;
+
         public double ElecEffect;
+
         public double IceEffect;
+
         public double WaterEffect;
+
         public double EarthEffect;
+
         public double MindEffect;
 
     }
@@ -24,7 +38,11 @@ public abstract class Entity {
     public static DefaultsStruct Defaults;
 
     public Vector2 Position { get; set; }
+
     public abstract int Health { get;  set; }
+
     public abstract int Mana { get; set; }
+
     public abstract int Speed { get; set; }
+
 }
