f612e0993f6324d49b451b10cd8a364f4ffd9543
ppebb <ppeb@ppeb.me>
2024-01-25 12:52:46 -0600

    Implemented UIContainer

--- src/Blackguard/UI/UIContainer.cs
+++ src/Blackguard/UI/UIContainer.cs
@@ -1,10 +1,9 @@
-using System.Collections.Generic;
-using Mindmagma.Curses;
+﻿using System.Collections.Generic;
 
 namespace Blackguard.UI;
 
 public class UIContainer : UIElement {
-    private List<UIElement> _elements;
+    private readonly List<UIElement> _elements;
     private int selected;
 
     // TODO: Handle sizing dynamically somehow
@@ -13,17 +12,70 @@ public class UIContainer : UIElement {
         _elements = elements;
     }
 
+    public UIContainer() {
+        _elements = new();
+    }
+
     public void Add(UIElement element) => _elements.Add(element);
 
     public void Remove(UIElement element) => _elements.Remove(element);
 
+    public bool Empty() => _elements?.Count == 0;
+
     public void Next() {
+        if (Empty())
+            return;
+
+        if (_elements[selected] is UIContainer container) {
+            if (!container.Bottom()) {
+                container.Next();
+                return;
+            }
+        }
+
         _elements[selected].Deselect();
-        
+
         if (++selected >= _elements.Count)
             selected = 0;
-        
+
         _elements[selected].Select();
+    }
+
+    public void Prev() {
+        if (Empty())
+            return;
+
+        if (_elements[selected] is UIContainer container) {
+            if (!container.Top()) {
+                container.Prev();
+                return;
+            }
+        }
+
+        _elements[selected].Deselect();
+
+        if (--selected < 0)
+            selected = _elements.Count - 1;
+
+        _elements[selected].Select();
+    }
+
+    public bool Top() {
+        bool top = selected == _elements.Count - 1;
+
+        if (_elements[selected] is UIContainer container)
+            return top && container.Top();
+
+        return top;
+    }
+
+    public bool Bottom() {
+        bool bottom = selected == _elements.Count - 1;
+
+        if (_elements[selected] is UIContainer container)
+            return bottom && container.Bottom();
+
+        return bottom;
     }
 
     // Called every tick
@@ -31,12 +83,12 @@ public class UIContainer : UIElement {
         if (Game.IsInput())
             ProcessInput();
     }
-    
-    // This implementation can likely be generalized to other things. Not sure yet
-    private int debounceTimer;
-    private List<int> inputBuffer = new();
 
     public override void ProcessInput() {
+        if (Empty())
+            return;
+
+        // I don't know if changing scenes within ProcessInput will result in a memory leak. Will have to profile later.
         _elements[selected].ProcessInput();
     }
 }
