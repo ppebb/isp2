20d76333a62250f865222a92f20826582b878a2b
Nathan-Goldman <111067824+Nathan-Goldman@users.noreply.github.com>
2024-01-29 12:35:52 -0600

    Added the big text for the main menu buttons. (#36)

--- src/Blackguard/UI/Scenes/Scene.cs
+++ src/Blackguard/UI/Scenes/Scene.cs
@@ -4,7 +4,7 @@ public abstract class Scene {
     public nint CurrentWin { get; protected set; }
 
     // Should be defined in the constructor inheriting Scene. Default container for the Scene where elements should be stored
-    private UIContainer container = null!;
+    protected UIContainer container = null!;
 
     // Returns false to exit the game
     public abstract bool RunTick();
