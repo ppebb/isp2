910dd760c5cbbc717614896882d3866cee77fba2
ppebb <ppeb@ppeb.me>
2024-02-26 12:48:13 -0600

    Add string extension method to convert an alphanumerical string to large ASCII text (#72, #6)

--- src/Blackguard/Utilities/Utils.cs
+++ src/Blackguard/Utilities/Utils.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 using Mindmagma.Curses;
 
 namespace Blackguard.Utilities;
 
-public static class CursesUtils {
+public static class Utils {
     public static void WindowAddLinesWithHighlight(nint window, params (Highlight highlight, int x, int y, string text)[] segments) {
         foreach ((Highlight highlight, int x, int y, string text) in segments) {
             try {
@@ -17,6 +20,68 @@ public static class CursesUtils {
     private static void mvwchgat(nint window, int x, int y, int len, uint attr, short pair) {
         NCurses.WindowMove(window, y, x);
         NCurses.WindowChangeAttribute(window, len, attr, pair, nint.Zero);
+    }
+
+    private static readonly string[] largeTextGlyphs = [
+        " ▄█▄ █▀▀▀▄▄▀▀▀▄█▀▀▄ █▀▀▀▀█▀▀▀▀▄▀▀▀▄█   █▀▀█▀▀  ▀█▀█  ▄▀█    █   █▄▀▀▀▄██  ██▀▀▀▄▄▀▀▀▄█▀▀▄ ▄▀▀▀▄▀▀█▀▀█   ██   ██   ██   ██   █▀▀▀▀█▄▀▀▀▄ ▄█  ▄▀▀▀▄▄▀▀▀▄  ▄█ █▀▀▀▀▄▀▀▀▄▀▀▀▀█▄▀▀▀▄▄▀▀▀▄     ",
+        " █ █ █▄▄▄▀█    █   ██▄▄▄ █▄▄▄ █    █▄▄▄█  █     █ █▄▀  █    ██ ███   ██ █ ██▄▄▄▀█   ██▄▄▀ ▀▄▄▄   █  █   █▀▄ ▄▀█ ▄ █ ▀▄▀  ▀▄▀   ▄▀ █▀▄ █  █     ▄▀  ▄▄▀▄▀ █ ▀▄▄▄ █▄▄▄    █ ▀▄▄▄▀▀▄▄▄█     ",
+        "█▀▀▀██   ██   ▄█  ▄▀█    █    █  ▀██   █  █  ▄  █ █ ▀▄ █    █ █ ██   ██ ▀▄██    █ ▀▄▀█  █ ▄   █  █  █   █ █ █ █ █ █▄▀ ▀▄  █  ▄▀   █  ▀█  █  ▄▀▀  ▄   █▀▀▀█▀▄   ██   █  █  █   █▄   █     ",
+        "▀   ▀▀▀▀▀  ▀▀▀ ▀▀▀  ▀▀▀▀▀▀     ▀▀▀ ▀   ▀▀▀▀▀▀ ▀▀  ▀   ▀▀▀▀▀▀▀ ▀ ▀ ▀▀▀ ▀  ▀▀▀     ▀▀ ▀▀   ▀ ▀▀▀   ▀   ▀▀▀   ▀   ▀ ▀ ▀   ▀  ▀  ▀▀▀▀▀ ▀▀▀ ▀▀▀▀▀▀▀▀▀▀ ▀▀▀    ▀  ▀▀▀  ▀▀▀  ▀    ▀▀▀  ▀▀▀      "
+    ];
+
+    // WARN: Terrible hardcode. Works well enough.
+    public static string[] ToLargeText(this string str) {
+        List<ReadOnlyMemory<char>[]> glyphs = new();
+
+        void AddGlyph(int start) {
+            glyphs.Add([
+                largeTextGlyphs[0].AsMemory(start, 5),
+                largeTextGlyphs[1].AsMemory(start, 5),
+                largeTextGlyphs[2].AsMemory(start, 5),
+                largeTextGlyphs[3].AsMemory(start, 5)
+            ]);
+        }
+
+        foreach (char c in str.ToLower()) {
+            if (c >= 48 && c <= 57) // Numbers 0-9
+                AddGlyph((c - 22) * 5);
+            else if (c >= 97 && c <= 127) // a-z
+                AddGlyph((c - 97) * 5);
+            else if (c == 32)
+                AddGlyph(36 * 5);
+        }
+
+        // This should work but doesn't. Would be nice to make this method somewhat less ugly.
+        /* StringBuilder[] ret = [ */
+        /*     new StringBuilder().AppendJoin(' ', glyphs[0]), */
+        /*     new StringBuilder().AppendJoin(' ', glyphs[1]), */
+        /*     new StringBuilder().AppendJoin(' ', glyphs[2]), */
+        /*     new StringBuilder().AppendJoin(' ', glyphs[3]), */
+        /* ]; */
+
+        StringBuilder[] ret = [new(), new(), new(), new()];
+
+        string format = "{0}";
+        bool set = false;
+        foreach (ReadOnlyMemory<char>[] mem in glyphs) {
+            ret[0].AppendFormat(format, mem[0]);
+            ret[1].AppendFormat(format, mem[1]);
+            ret[2].AppendFormat(format, mem[2]);
+            ret[3].AppendFormat(format, mem[3]);
+
+            // Kind of hacky. Oh well
+            if (!set) {
+                format = " {0}";
+                set = true;
+            }
+        }
+
+        return [
+            ret[0].ToString(),
+            ret[1].ToString(),
+            ret[2].ToString(),
+            ret[3].ToString()
+        ];
     }
 }
 #pragma warning restore IDE1006
